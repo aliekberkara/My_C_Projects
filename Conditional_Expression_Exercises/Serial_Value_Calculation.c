858ec30737d9a9fc9cecb558d7fa508227d274c1
aliekberkara <79039580+aliekberkara@users.noreply.github.com>
2021-11-09 18:23:47 +0300

    Update Serial_Value_Calculation.c

--- Conditional_Expression_Exercises/Serial_Value_Calculation.c
+++ Conditional_Expression_Exercises/Serial_Value_Calculation.c
@@ -17,7 +17,8 @@ int main()
 {
     int n, i;
     float x, total;
-
+    char continue1;
+    do{
     printf("Enter a Positive Integer: \n");
     scanf("%d", &n);
     printf("Enter a Real Number: \n");
@@ -27,12 +28,16 @@ int main()
         for(i = 1; i <= 2 * n - 1; i += 2){
         total += i/(pow(x, i+1));
         }
-        printf("Total Of The Series: %.3f", total);
+        printf("Total Of The Series: %.3f\n", total);
+        printf("\nDo you want to continue (Evet: E/e)\n");
+        scanf("%s", &continue1);
+        printf("---------------------------\n");
     }
     else{
         printf("Invalid Number");
         }
-
+    } while((continue1 == 'E') || (continue1 == 'e'));
+    printf("See You!");
 
     return 0;
 }
