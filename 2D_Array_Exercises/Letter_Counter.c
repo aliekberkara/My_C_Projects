41f5c3aa220e0eaa6ebf31f4ae0e654fe4a33bbf
aliekberkara <79039580+aliekberkara@users.noreply.github.com>
2021-11-14 22:24:09 +0300

    Create Letter_Counter.c

---
+++ 2D_Array_Exercises/Letter_Counter.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+--> Ask the user to enter a sentence.
+--> Print the number of times the letters in the entered sentence are written on the screen.
+*/
+
+int main()
+{
+	char c;
+	int counter[26] = {0}, letter, character = 0, i;
+	printf("Enter a sentence: ");
+	while((c = getchar()) != '\n'){
+		letter = c - 'a';
+		if(letter >= 0 && letter < 26){
+			counter[letter]++;
+		}
+		else{
+			character++;
+		}
+	}
+	printf("\nThe sentence you entered contains %d characters.", character);
+	i = 0;
+	while(i < 26){
+		printf("%d kez %c\n", counter[i], 'a'+i);
+		i++;
+	}
+
+    return 0;
+}
