03c9b125a91c0a806d119244a9dba899de866ce3
aliekberkara <79039580+aliekberkara@users.noreply.github.com>
2021-12-03 10:26:02 +0300

    Create Point.c

---
+++ Struct_Exercises/Point.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+- Create a struct named 'Point'. ABS (location on line X) ORD (location on line Y), make these values float type.
+- Create a point named P.
+- Assign the P point abs : 3 and ord : 2.
+- Print the point p to the screen in printf.
+*/
+
+struct point{
+float abs, ord;
+};
+
+int main()
+{
+    struct point P;
+    P.abs = 3;
+    P.ord = 2;
+    printf("(x,y) = (%.1f, %.1f)\n-------------------", P.abs, P.ord);
+
+    return 0;
+}
