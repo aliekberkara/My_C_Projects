0c57934e652e4fd604837694487a972eeddf6d95
aliekberkara <79039580+aliekberkara@users.noreply.github.com>
2021-12-07 12:18:28 +0300

    Create Prime_Numbers.c

---
+++ Recursive_Function_Exercises/Prime_Numbers.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Write the C program with the help of the recursive function,
+which allows the prime numbers up to an N positive integer entered from the keyboard to be listed on the screen.
+*/
+
+int Is_Prime(int number,int i)
+{
+    if(i==1)
+    {
+        return 1;
+    }
+    else if(number%i==0)
+    {
+        return 0;
+    }
+    else
+    {
+        Is_Prime(number,i-1);
+    }
+}
+
+
+
+
+int main()
+{
+
+
+    int number,i,control;
+    printf("Enter a number: ");
+    scanf("%d",&number);
+    for(i=2;i<=number;i++)
+    {
+        control=Is_Prime(i,i/2);
+        if(control==1)
+        {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
