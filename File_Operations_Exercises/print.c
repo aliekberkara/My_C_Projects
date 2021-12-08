ae93a36d0bb2d5ab9be89fdc0337b0f2d971de34
aliekberkara <79039580+aliekberkara@users.noreply.github.com>
2021-12-08 06:44:00 +0300

    create

---
+++ File_Operations_Exercises/print.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Write a C program that allows the information in the print.txt file to be read character by character and displayed on the screen.
+*/
+
+int main()
+{
+
+    FILE *file;
+    char character;
+    file = fopen("print.txt", "r");
+    if(file == NULL){
+		printf("Failed to Create File.");
+    }
+    else{
+		while(character != EOF){
+			character = getc(file);
+			if(character != EOF){
+				printf("%c", character);
+			}
+		}
+    }
+    printf("\n------------------------------");
+    fclose(file);
+    return 0;
+}
