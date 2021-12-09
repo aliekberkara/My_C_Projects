1cfadbb1fad6225685af47bc740839a21995db65
aliekberkara <79039580+aliekberkara@users.noreply.github.com>
2021-12-09 13:44:33 +0300

    Create Changing_Numbers.c

---
+++ Dynamic_Memory_Management_Exercises/Changing_Numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Space is allocated in different memory areas for two integers entered by the user.
+Accordingly, write a C program that allows the numbers in these two memory regions to be swapped.
+*/
+
+int main()
+{
+	int *number1, *number2, *temp;
+	number1 = (int *) malloc(sizeof(int));
+	number2 = (int *) malloc(sizeof(int));
+
+    printf("Enter the 1st number: ");
+    scanf("%d", number1);
+    printf("Enter the 2nd number: ");
+    scanf("%d", number2);
+    printf("-------------------------------\n");
+
+    printf("First number: %d Second number: %d\n", *number1, *number2);
+    printf("-------------------------------\n");
+
+    temp = number1;
+    number1 = number2;
+    number2 = temp;
+    printf("First number: %d Second number: %d\n", *number1, *number2);
+    printf("-------------------------------\n");
+    return 0;
+}
